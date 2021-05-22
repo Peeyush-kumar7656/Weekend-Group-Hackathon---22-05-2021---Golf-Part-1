64dce22781fe5b737442c48d13dea3b43bbfa129
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-22 16:41:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 const App = () => {
     const [renderBall, setRenderBall] = useState(false);
     const [posi, setPosi] = useState(0);
